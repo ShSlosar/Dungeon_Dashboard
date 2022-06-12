3baa845e301dd794bbf94547ac8bd4092926e4e1
Shelby Slosar <shelby.slosar@protonmail.com>
2022-06-12 09:16:36 -0700

    changes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask_app.controllers import users, game_cont, clock_cont
+from flask_app.controllers import users, game_cont, clock_cont, note_cont, monster_cont,character_cont
 from flask_app import app
 
 if __name__ == "__main__":
